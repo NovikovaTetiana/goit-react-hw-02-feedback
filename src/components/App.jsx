aa43d1b09c3d33055c8ca84cb4b8377873914934
Tetiana Novikova <113465579+NovikovaTetiana@users.noreply.github.com>
2023-05-09 00:35:32 +0300

    hw-2 start doing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+// import { Header } from "./Header";
+// import LeaveFeedback from "./LeaveFeedback";
+
+
+ export class App extends Component{
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+
+
+  render(){
+    return(
+<div>
+<h1>Please leave feedback</h1>
+</div>
+    )
+  }
+}
---
+++ src/components/LeaveFeedback/LeaveFeedback.jsx
@@ -0,0 +1,20 @@
+// import PropTypes from 'prop-types';
+
+// export default function LeaveFeedback({ options, onLeaveFeedback }) {
+//   return (
+//     <ul>
+//       {options.map(el => (
+//         <li key={el}>
+//           <button onClick={() => onLeaveFeedback(el)}>{el}</button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+// LeaveFeedback.propTypes = {
+//   onLeaveFeedback: PropTypes.func.isRequired,
+//   options: PropTypes.arrayOf(PropTypes.string).isRequired,
+// };
+
+
