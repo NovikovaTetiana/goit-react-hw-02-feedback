8d023b713139cb55cd1f391509400bca36713bb9
Tetiana Novikova <113465579+NovikovaTetiana@users.noreply.github.com>
2023-05-09 10:47:13 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,45 @@
-import { Component } from "react";
-import { Header } from "./Header";
-import FeedbackOptions from "./FeedbackOptions";
+import { Component } from 'react';
+import { Header } from './Header';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section/Section';
+import { Statistics } from './Statistics';
 
-
- export class App extends Component{
-
+export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
   }
 
-countTotalFeedback(){
-  const {good, neutral, bad} = this.state;
-  return good + neutral + bad
-}
+  countPositiveFeedbackPercentage({ good }) {
+    Math.round((good * 100) / this.countTotalFeedback(this.state));
+  }
 
-countPositiveFeedbackPercentage({good} ){
-  Math.round((good*100)/this.countTotalFeedback(this.state))
+  handelLeaveFeedback = el => this.setState(prev => ++prev[el]);
 
-}
-
-handelLeaveFeedback = el => this.setState(prev => ++prev[el]);
-
-  render(){
-    return(
-<div>
-<Header title="Please leave feedback" />
-<FeedbackOptions options={['good','neutral', 'bad']} onLeaveFeedback={this.handelLeaveFeedback}/>
-</div>
-)
+  render() {
+    return (
+      <div>
+        <Header title="Please leave feedback" />
+        <Section> <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={this.handelLeaveFeedback}
+        /></Section>
+        <Section title="Statistics" />
+        <Statistics
+         {...this.state}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        /><Statistics/>
+        <Section/>
+      </div>
+    );
   }
 }
+
+export default App;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+export const Notification = ({msg}) => {
+  return (
+    <p>{msg}</p>
+  )
+}
+
+Notification.propTypes = {
+  msg: PropTypes.string.isRequired
+}
